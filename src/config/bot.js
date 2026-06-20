88ec4b0f3b9dc92fa16f0720a8547bbceeba8953
wannnito <james097sol@gmail.com>
2026-06-20 08:06:26 -0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "sushikimchi19",
+        name: "@sushikimchi19",
         // Activity type number (0 = Playing).
         type: 1,
       },
